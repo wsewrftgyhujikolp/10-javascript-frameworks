ca54d4d773491d43d43f449d70dddf70c50b5629
Dominic Müller <dominic.mueller@talentwerk.ch>
2024-03-19 11:59:51 +0100

    Added checking off functionality for TODO items, fixed error when having same text in multiple items

--- vue-app/src/components/HelloWorld.vue
+++ vue-app/src/components/HelloWorld.vue
@@ -2,7 +2,9 @@
   <div>
 
     <ul>
-      <li v-for="todo in todos" v-bind:key="todo">{{ todo }}</li>
+      <li v-for="todo in todos" v-bind:key="todo.id"> <!-- use a real id field (todo.id) to prevent problems when todos have the same text -->
+        <input type="checkbox" v-model="todo.done"><!-- checkbox for marking as done, bind to the "done" field in vuejs model -->
+        {{ todo.text }}</li>
     </ul>
 
     <form v-on:submit.prevent="addTodo">
@@ -24,9 +26,22 @@ export default {
   },
   methods: {
     addTodo: function () {
-      this.todos = [...this.todos, this.todoText];
-      localStorage.setItem('todos', JSON.stringify(this.todos));
+      var newTodo = {
+        text: this.todoText,
+        done: false,
+        id: Date.now(), // not great, but good enough for this example
+      }
+      this.todos.push(newTodo);
     },
+  },
+  watch: { // überwachungen
+       todos: { //welches objekt überwacht wird: "todos" liste
+        handler(oldTodos, newTodos) {
+          console.log("detected update of todos:", oldTodos, newTodos);
+          localStorage.setItem('todos', JSON.stringify(newTodos));  // todos neu speichern
+        },
+        deep: true, // änderungen des kompletten todo objekts werden überwacht
+      },
   },
   mounted: function () {
     const existingTodos = localStorage.getItem('todos');

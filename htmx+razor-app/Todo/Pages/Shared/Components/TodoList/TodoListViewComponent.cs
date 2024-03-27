0763d4a5d37886f76746d386168f022b6b4cd833
Dominic Müller <dominic.mueller@talentwerk.ch>
2024-03-27 12:57:28 +0100

    Added asp.net todo app, wip

---
+++ htmx+razor-app/Todo/Pages/Shared/Components/TodoList/TodoListViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+
+public class CardViewComponent : ViewComponent
+{
+   
+    public IViewComponentResult Invoke()
+    {
+        foreach (KeyValuePair<string, object> item in this.ViewData)
+        {
+            Console.WriteLine(item.Key + " " + item.Value);
+        }
+
+        return View(new TodoListViewModel()
+        {
+            Todos = new List<TodoItem>()
+            {
+                new TodoItem() { Text = "Learn .NET Core", IsDone = true, Id = 1 },
+            }
+        });
+    }
+}
---
+++ htmx+razor-app/Todo/Pages/Shared/Components/TodoList/TodoListViewModel.cs
@@ -0,0 +1,11 @@
+﻿public class TodoListViewModel
+{
+    public List<TodoItem> Todos { get; set; }
+}
+
+public class TodoItem
+{
+    public string Text { get; set; }
+    public bool IsDone { get; set; }
+    public int Id { get; set; }
+}
